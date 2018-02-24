197104f4334ec3769156c28baac30c7c888bac18
KatalinicD <katalind@unbc.ca>
2018-02-23 16:43:47 -0800

    2.0

---
+++ Score 4/src/GameElements/Location.java
@@ -0,0 +1,19 @@
+package GameElements;
+
+public class Location {
+	public static final int MAX_ROW=3;
+	public static final int MIN_ROW=0;
+	public static final int MAX_COLUMN=3;
+	public static final int MIN_COLUMN=0;
+	private int row;
+	private int column;
+	
+	public int getRow() {
+		return row;
+	}
+	public int getColumn() {
+		return column;
+	}
+	
+
+}
