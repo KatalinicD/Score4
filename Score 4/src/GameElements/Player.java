72d656932302af1e9b6889c8e2d1d88b0f957dce
JeyePixel <heales@unbc.ca>
2018-02-23 17:43:30 -0800

    Add files via upload

--- Score 4/src/GameElements/Player.java
+++ Score 4/src/GameElements/Player.java
@@ -1,18 +1,45 @@
 package GameElements;
 
 public class Player {
-	private int id;
-	private Bead[] set;
-	private Colour colour;
 	
-	public Player (int identity) {
-		id=identity;
-		set=new Bead[32];
-		for (int i=0; i<32; i++) {
-			set[i]=new Bead(i+1);
-			}
-		colour=Colour.Black;
+	private final int id;
+	//private final Colour colour; Requires a Colour enum
+	
+	//Player constructor
+	public Player(int identification){
+		id = identification;
+		//Colour = null; This will require the Referee to call startGameAs(Colour c)
 	}
-
 	
+	public void reset(){
+		//This method will erase the last recorded Location 
+	}
+	
+	public void noteOpponentsId(int id){
+		//Not sure if we need this information for anything, but we need the method
+	}
+	
+	/*public Location requestMoveLocation(){
+	 * Requires the Location object
+	 * Will be altered in AiPlayer class
+	  }
+	 */
+	
+	/*public Location retry(){
+	*This will call the requestMoveLocation() method once again
+	*Maybe have output telling the player to try again? If so, don't need 
+	*output for AiPlayer
+	}
+	*/
+	
+	/*public void opponentPlays(Location ell){
+	*This will tell us the last position the opponent played in
+	}
+	*/
+	
+	/*public void noteGameOver(GameOverStatus ...){
+	*Require the GameOverStatus enum
+	*	
+	}
+	*/
 }
