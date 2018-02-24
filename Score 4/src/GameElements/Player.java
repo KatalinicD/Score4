92ac7335df5ac065624063b46dc1a4164ebd74ee
KatalinicD <katalind@unbc.ca>
2018-02-23 17:12:14 -0800

    2.0

--- Score 4/src/GameElements/Player.java
+++ Score 4/src/GameElements/Player.java
@@ -1,15 +1,18 @@
 package GameElements;
 
 public class Player {
-	private String name;
+	private int id;
 	private Bead[] set;
+	private Colour colour;
 	
-	public Player (String identity) {
-		name=identity;
+	public Player (int identity) {
+		id=identity;
 		set=new Bead[32];
 		for (int i=0; i<32; i++) {
 			set[i]=new Bead(i+1);
 			}
+		colour=Colour.Black;
 	}
 
+	
 }
---
+++ Score 4/src/GameElements/Tester.java
@@ -0,0 +1,9 @@
+package GameElements;
+
+public class Tester {
+	public static void main (String[]args) {
+		Player player=new Player(10000);
+		player.
+	}
+
+}
