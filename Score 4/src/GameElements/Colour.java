318d83334855ebcae426ca8db603304a5de6ef50
KatalinicD <katalind@unbc.ca>
2018-02-23 17:07:42 -0800

    2.1

--- Score 4/src/GameElements/Colour.java
+++ Score 4/src/GameElements/Colour.java
@@ -1,5 +1,18 @@
 package GameElements;
 
 public enum Colour {
-    Black, White, None;
+    Black, White;
+	private Colour colour;
+	private boolean isBlack;
+	private boolean isWhite;
+	
+	public boolean isBlack() { 
+		if (this.colour==Black) {
+	    return true; }
+		else return false;
+		
+	}
+	
+
+	
 }
