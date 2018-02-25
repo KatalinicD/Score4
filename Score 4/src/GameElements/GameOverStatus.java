d1cbbc5268fae0d79324c7539c7950636a9d917c
KatalinicD <katalind@unbc.ca>
2018-02-25 12:17:56 -0800

    2.2

--- Score 4/src/GameElements/GameOverStatus.java
+++ Score 4/src/GameElements/GameOverStatus.java
@@ -1,7 +1,22 @@
 package GameElements;
 
+// this stores the state of the game //
 public enum GameOverStatus {
 	
 	WIN, LOSE, DRAW;
 
+	// this method checks if the state of the game is a WIN //
+	public boolean isWin() {
+		return this==WIN;
+	}
+	
+	// this method checks if the state of the game is a LOSE //
+	public boolean isLose() {
+		return this==LOSE;
+	}
+	
+	// this method checks if the state of the game is a DRAW //
+	public boolean isDraw() {
+		return this==DRAW;
+	}
 }
