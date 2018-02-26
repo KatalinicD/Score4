f1bd3de933439eb20cd2421203be940cc9c48090
kaori <hanakosinclare@gmail.com>
2018-02-26 09:10:17 -0800

    Add files via upload

---
+++ Score 4/src/Board.java
@@ -0,0 +1,29 @@
+package score4_gui;
+
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+
+public class Board {
+	
+	private int xPosition;
+	private int yPosition;
+	
+	public Board(int x, int y){
+		
+		xPosition = x;
+		yPosition = y;
+		
+	}
+	
+	//Draw a board
+	public void draw(Graphics2D g){
+		
+		
+	}
+	
+	
+
+}
---
+++ Score 4/src/BoardViewer.java
@@ -0,0 +1,16 @@
+package score4_gui;
+
+import javax.swing.JFrame;
+
+public class BoardViewer {
+	
+	public static void main(String[] args){
+	
+		JFrame frame = new JFrame();
+	
+		frame.setSize(0,0);
+		frame.setTitle("");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	}
+}
