b1101c73328c677737d1f04265b43253f104b70c
KatalinicD <katalind@unbc.ca>
2018-02-26 14:43:06 -0800

    2.2

--- Score 4/src/GameElements/Location.java
+++ Score 4/src/GameElements/Location.java
@@ -1,5 +1,6 @@
 package GameElements;
 
+// this is the location class, which defines the location of a peg on a board //
 public class Location {
 	public static final int MAX_ROW=3;
 	public static final int MIN_ROW=0;
@@ -8,9 +9,18 @@ public class Location {
 	private int row;
 	private int column;
 	
+	// this creates a new location //
+	public Location (int Row, int Column) {
+		row=Row;
+		column=Column;	
+	}
+	
+	// this returns the row number of a location //
 	public int getRow() {
 		return row;
 	}
+	
+	// this returns the column number of a column //
 	public int getColumn() {
 		return column;
 	}
