32f0d8a65e7f218ccfbc06a625b3f31846f57b07
KatalinicD <katalind@unbc.ca>
2018-03-02 16:53:07 -0800

    Final

--- Score 4/src/ca/unbc/cpsc/team_breve/Bead.java
+++ Score 4/src/ca/unbc/cpsc/team_breve/Bead.java
@@ -1,9 +1,14 @@
 package ca.unbc.cpsc.team_breve;
 
+// this class makes a bead //
 public class Bead {
 	private Colour beadcolour;
+	// this constructs a bead
 	public Bead(Colour colour) {
 	    beadcolour=colour;
 	}
-
+    // this returns the colour of the bead //
+	public Colour getColour() {
+		return this.beadcolour;
+	}
 }
