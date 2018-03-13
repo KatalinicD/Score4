b560528d270e02e72f1fca30a75ac979d8f1ff16
KatalinicD <36277734+KatalinicD@users.noreply.github.com>
2018-03-13 10:20:22 -0700

    Add files via upload

--- Score 4/src/ca/unbc/cpsc/team_breve/Board.java
+++ Score 4/src/ca/unbc/cpsc/team_breve/Board.java
@@ -1,10 +1,11 @@
 package ca.unbc.cpsc.team_breve;
 // this class creates a board //
 public class Board {
-	private int pegnumber;
+	
 	private int length=Location.MAX_ROW+1;
 	private int width=Location.MIN_COLUMN+1;
 	private Peg [][] board;
+	
 	// this creates a new board
 	public Board() {
 		board=new Peg [length][width];
@@ -24,4 +25,49 @@ public class Board {
 		return board [r][c];
 	}
 	
+	// this adds a bead to a peg on the board if possible //
+	public void addBead(int row, int column, Colour colour) {
+		board[row][column].addBead(colour);
+	}
+	
+	// this method removes all beads from all pegs on the board //
+	public void clearBoard() {
+		for (int i=0; i<length; i++) {
+			for (int j=0; j<width; j++) {
+				board[i][j].removeBeads();
+			}
+		}
+	}
+	
+	// this returns the first empty bead //
+	public Bead checkPeg (int row, int column) {
+		if (board[row][column].getBead(0)==null) {
+			return board[row][column].getBead(0);
+		}
+		else if (board[row][column].getBead(1)==null) {
+			return board[row][column].getBead(1);
+		}
+		else if (board[row][column].getBead(2)==null) {
+			return board[row][column].getBead(2);
+		}
+		else return board[row][column].getBead(3);
+	}
+
+	// this returns the first spot that has a bead  //
+	public Bead checkMove(int row, int column) {
+		if (board[row][column].getBead(0)!=board[row][column].getBead(1)) {
+			return board[row][column].getBead(0);
+		}
+		else if (board[row][column].getBead(1)!=board[row][column].getBead(2)) {
+			return board[row][column].getBead(1);
+		}
+		else if (board[row][column].getBead(2)!=board[row][column].getBead(3)) {
+			return board[row][column].getBead(2);
+		}
+		else if (board[row][column].getBead(2)!=null && board[row][column].getBead(3)!=null) {
+			return board[row][column].getBead(3);
+		}
+		else return board[row][column].getBead(0);
+		
+	}
 }
