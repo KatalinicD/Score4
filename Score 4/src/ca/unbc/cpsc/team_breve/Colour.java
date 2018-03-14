cefe4207a6216ae05cac85db7b56177a0687dc30
KatalinicD <36277734+KatalinicD@users.noreply.github.com>
2018-03-14 16:58:56 -0700

    Final

--- Score 4/src/ca/unbc/cpsc/team_breve/Colour.java
+++ Score 4/src/ca/unbc/cpsc/team_breve/Colour.java
@@ -4,11 +4,11 @@ package ca.unbc.cpsc.team_breve;
 public enum Colour {
 	Black,White;
 	
-	// this checks if the colour is black //
+	// this checks if the colour is black  //
 	public boolean isBlack() { 
 		return this==Black;	
 	}
-	// this checks if the colour is white //
+	// this checks if the colour is white  //
 	public boolean isWhite() {
 		return this==White;
 	}
