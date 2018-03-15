9c6755db357b023f7636c9a029ae05d7d552d905
JeyePixel <heales@unbc.ca>
2018-03-14 17:01:22 -0700

    Final Test
    The AIPlayer class lacks any GameStateException try/catch/throws, need to also modify package name (Maybe?)

--- Score 4/src/ca/unbc/cpsc/team_breve/AIPlayer.java
+++ Score 4/src/ca/unbc/cpsc/team_breve/AIPlayer.java
@@ -1,10 +1,66 @@
 package ca.unbc.cpsc.team_breve;
 
-public class AIPlayer {
-	private String id;
-	
-	public AIPlayer (String identifier) {
-	id=identifier;
-	}
+import java.util.Random;
 
+public class AIPlayer extends Player{
+	
+	//Random object to set our ID
+	Random rand = new Random();
+	
+	//This boolean will record the Referee called retry() 
+	private boolean retried = false;//Starts out false because retry() hasn't been called
+	
+	//This will be the AIplayers copy of the board
+	Board boardCopy;
+	
+	//Constructor
+	public AIPlayer () { boardCopy = new Board();}//Create a copy board
+		
+	//We need to override the reset command to include resetBoardCopy()
+	@Override
+	public void reset() {
+		super.reset();//Calls reset() from the superclass, Player
+		boardCopy.clearBoard();
+	}
+	
+	//We need to override the requestMoveLocation from Player. AIplayer has to choose independent of input
+	@Override
+	public Location requestMoveLocation() {
+		int row;
+		int column;
+		if(retried) {//If retry() was called, do this
+			row = rand.nextInt(3) + 0;
+			column = rand.nextInt(3) + 0;
+			Location location = new Location(row, column);
+			retried = false;
+			return location;
+		}
+		else {//If retry() was not called, do this
+			return this.getOpponentsLastLocation();
+		}
+	}
+	
+	//The override removes the System.out.println statement present in Player
+	@Override
+	public Location retry() { 
+		retried = true;//Note that the retry() method was called
+		return this.requestMoveLocation();
+	}
+	
+	//This method places a bead onto the AIPlayer's copy of the board
+	public void placeCopyBead(int ident, Location loc) {
+		int row;//Get the row and column of the Location argument
+		int column;
+		row = loc.getRow();
+		column = loc.getColumn();
+		if(this.getId() == ident) {boardCopy.addBead(row,column,this.getColour());}//Place Players bead
+		else {boardCopy.addBead(row, column, this.getOpponentColour());}//Place Opponents bead		
+	}
+	//The override adds a placeCopyBead() method 
+	@Override
+	public void opponentPlays(Location ell) {
+		super.opponentPlays(ell);
+		//Add the placeCopyBead method here
+	}
+	
 }
