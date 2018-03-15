4d30c0bcea7e99b8b1637696aed684b2d372dffb
JeyePixel <heales@unbc.ca>
2018-03-14 17:05:24 -0700

    Final Test
    The Player class lacks any GameStateException throw/try/catches. Also, check package names

--- Score 4/src/ca/unbc/cpsc/team_breve/Player.java
+++ Score 4/src/ca/unbc/cpsc/team_breve/Player.java
@@ -2,40 +2,88 @@ package ca.unbc.cpsc.team_breve;
 
 import java.util.Scanner;
 
-public class Player{
+import ca.unbc.cpsc.team_breve.enums.*;
+import java.util.Random;
+
+public class Player {
 	
 	//Scanner is needed for the Player.requestMoveLocation()
 	Scanner in = new Scanner(System.in);
+	//Random is needed for the player id
+	Random rand = new Random();
 	
 	//Member variables
-	int id;
-	Colour colour;
-	int opponentsId;
-	GameOverStatus gamestatus;
-	Location opponentsLastLocation;
+	private int id;
 	
+	//Getter method for id to use with AIPlayer
+	public int getId() {
+		return this.id;
+	}
 	
+	private Colour colour;
+	
+	//Getter method for our Colour to be used with AIPlayer
+	public Colour getColour() {
+		return this.colour;
+	}
+	
+	private int opponentsId;
+	
+	//Getter method for the opponents id to use with AIPlayer
+	public int getOpponentsId() {
+		return this.opponentsId;
+	}
+	
+	private Colour opponentsColour;
+	
+	//Getter method for the opponents Colour
+	public Colour getOpponentColour() {
+		return this.opponentsColour;
+	}
+	
+	private GameOverStatus gamestatus;
+	
+	private Location opponentsLastLocation;
+	
+	//Getter method for the opponentsLastLocation
+	public Location getOpponentsLastLocation() {
+		return this.opponentsLastLocation;
+	}
+		
 	//Player constructor
-	public Player(int identification){
-		id = identification;
+	public Player(){
+		id = rand.nextInt(Integer.MAX_VALUE) + 0;
 		colour = null; //A player object will be assigned a color by the referee
 		gamestatus = null;
 		opponentsLastLocation = null;
+		opponentsColour = null;
+		}
+	
+	//The referee will use this method to assign our Player a Colour
+	public void startGameAs(Colour c) {
+		colour = c;
+		opponentsColour = (colour == Colour.White) ? Colour.Black: Colour.White;//Sets the opponents Colour
 	}
 	
-	public void reset(){
-		id = 0;
+	//This method resets the Player 
+	public void reset() {
+		id = rand.nextInt(Integer.MAX_VALUE) + 0;
 		colour = null;
 		opponentsId = 0;
 		gamestatus = null;
 		opponentsLastLocation = null;
 	}
-	
-	public void noteOpponentsId(int id){
+		
+	//This method will record the ID of the opponent. Not sure what we want to do with it yet
+	public void noteOpponentsId(int id) {
 		opponentsId = id;
+		if(this.getOpponentsId() == id) {//Checks to make sure the PLayer doesn't share their id with the opponent
+			id = rand.nextInt(Integer.MAX_VALUE) + 0;
+		}
 	}
 	
-	public Location requestMoveLocation(){
+	//The Referee will use this to get a Location from the PLayer. Will be different for AIplayer
+	public Location requestMoveLocation() {
 		int x;
 		int y;
 		System.out.println("Please enter a row number: ");
@@ -46,17 +94,28 @@ public class Player{
 		Location moveLocation = new Location((x - 1),(y - 1));
 		return moveLocation;
 	}
-	 
-	public Location retry(){
+	
+	//Referee calls this method if our Location could not accept a bead
+	public Location retry() {
 		System.out.println("Please enter a new location.");
 		return this.requestMoveLocation();
 	}
 	
-	public void opponentPlays(Location ell){
-		//Need to work on this
+	//Notes the last Location that the opponent played. not sure what we want to do with this yet
+	public void opponentPlays(Location ell) {
+		opponentsLastLocation = ell;
 	}
 	
-	public void noteGameOver(GameOverStatus gameoverstatus){
+	//Referee sends the Player a GameOverStatus when the game ends. 
+	public void noteGameOver(GameOverStatus gameoverstatus) {
 		gamestatus = gameoverstatus;
 	}
+	
+	//This is the getAI method. Not sure if it belong in Player
+	public Player getAI() {
+		Player player = new AIPlayer();
+		return player;
+	}
+	
+	
 }
