14fb75784fa3ccbdf17f4f79160492dd390cb22b
KatalinicD <36277734+KatalinicD@users.noreply.github.com>
2018-03-16 17:24:11 -0700

    1

--- Score 4/src/ca/unbc/cpsc/team_breve/Game.java
+++ Score 4/src/ca/unbc/cpsc/team_breve/Game.java
@@ -1,12 +1,31 @@
 package ca.unbc.cpsc.team_breve;
 
 public class Game {
+	private Board board;
+	private Referee referee;
+	private Player human;
+	private Player opponent;
+	
 	public Game () {
 		Board board=new Board();
-		Player a=new Player(10);
-		Player b=new Player(20);
-		
-		
+		Referee referee=new Referee();
+		Player human=new Player();
+		Player opponent=new AIPlayer();
 	}
-
+    
+	public Board getBoard() {
+		return board;
+	}
+	
+	public Board getReferee() {
+		return referee;
+	}
+	
+	public Player getHuman() {
+		return human;
+	}
+	
+	public Player getOpponent() {
+		return opponent;
+	}
 }
