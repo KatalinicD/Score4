dc71012dfbf23ffeb8b41b5a9d3ce3a6567f54e2
Brandon <mario@DESKTOP-KUIF47F.hitronhub.home>
2018-03-20 03:25:15 -0700

    Small update to referee with checkGameState and to Game class.

--- Score 4/src/ca/unbc/cpsc/team_breve/Game.java
+++ Score 4/src/ca/unbc/cpsc/team_breve/Game.java
@@ -1,8 +1,20 @@
 package ca.unbc.cpsc.team_breve;
 
 public class Game {
-	public Game () {
-		Board board=new Board();
+	
+		Board board = new Board();
+		Referee ref = new Referee();
+		Player human = new Player();
+		Player opponent = new AIPlayer();
+		Gui gui = new GUI();	
+	
+	//Main method to start the game
+	public static void main (String []args)
+	{
+		
+		
+		
+		
 	}
 
 }
--- Score 4/src/ca/unbc/cpsc/team_breve/Referee.java
+++ Score 4/src/ca/unbc/cpsc/team_breve/Referee.java
@@ -1,11 +1,41 @@
 package ca.unbc.cpsc.team_breve;
 
 public class Referee {
-     private String name;
-     public Referee(String identity) {
-    	 name=identity;
+     
+	
+	
+    
+	public Referee() 
+	{
+		
+	}
+	
+     //2 ideas, one with board, players and ref called in game class and other with just everything under ref class so far
+     
+     public void start(Player human, AIPlayer opponent) {
+    	 
+    	 human.startGameAs(White);
+    	 opponent.startGameAs(Black);
+    	 
      }
-     public void start() {
+     
+     
+     
+     public void reset(Player human, AIPlayer opponent, Board gameboard)
+     {
+    	 gameboard.clearBoard();
+    	 human.reset();
+    	 opponent.reset();
+    	 
+    	 human.startGameAs(White);
+    	 opponent.startGameAs(Black);
+     }
+     
+     public void checkGameState(Board gameboard)
+     {
+    	 gameboard.checkBoard;
+    	 
+    	 
     	 
      }
 }
