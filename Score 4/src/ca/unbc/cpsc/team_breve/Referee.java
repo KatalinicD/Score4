024fec5cf866f54f8f16bb5bc6b7c3aa708ca7da
kaori <hanakosinclare@gmail.com>
2018-03-22 17:43:30 -0700

    Add files via upload

--- Score 4/src/ca/unbc/cpsc/team_breve/Referee.java
+++ Score 4/src/ca/unbc/cpsc/team_breve/Referee.java
@@ -1,19 +1,82 @@
-package ca.unbc.cpsc.team_breve;
-
-import java.util.Random;
+package trial;
 
 public class Referee {
-     private int id;
+    
+	Board gameboard;
+	Player human;
+	AIPlayer opponent;
+	Gui gui;
+	Location savedLocation = null;
+	
+	public Referee(Board board, Player player, Player ai, Gui guiMethods) 
+	{
+			gameboard = board;
+			human = player;
+			opponent = (AIPlayer) ai;
+			gui = guiMethods;
+	}
+	
+     //2 ideas, one with board, players and ref called in game class and other with just everything under ref class so far
      
-     public Referee(int identification) {
-    	 id=identification;
-     }
      public void start() {
-    	    Random rand=new Random();
-    	    int x=rand.nextInt(1001);
-    	    int y=rand.nextInt(1001);
-    	    Player player=new Player(x);
-    	    Player opponent=new Player(y);
-    	    Board board=new Board();
+    	 
+    	 human.startGameAs(Colour.White);
+    	 opponent.startGameAs(Colour.Black);
+    	 //currently preset like this until we get buttons in the GUI
+    	 
+     }
+     
+     
+     public void reset()
+     {
+    	 gameboard.clearBoard();
+    	 human.reset();
+    	 opponent.reset();
+    	 
+    	 savedLocation = null;
+    	 
+    	 human.startGameAs(Colour.White);
+    	 opponent.startGameAs(Colour.Black);
+     }
+     
+     public GameOverStatus checkGameState()
+     {
+    	 if (gameboard.checkBoard(human.getColour()))
+    	 {
+    		 human.noteGameOver(GameOverStatus.WIN);
+    		 opponent.noteGameOver(GameOverStatus.LOSE);
+    		 return GameOverStatus.WIN;
+    		 //
+    	 }
+    	 else if (gameboard.checkBoard(opponent.getColour()))
+    	 {
+    		 human.noteGameOver(GameOverStatus.LOSE);
+    		 opponent.noteGameOver(GameOverStatus.WIN);
+    		 return GameOverStatus.LOSE;
+    	 }
+    	 
+    	 return null;
+     }
+     
+     public void saveLocation (Location loc)
+     {
+    	 savedLocation = loc;
+     }
+     
+     public Location getSavedLocation()
+     {
+    	 return savedLocation;
+     }
+     
+     public void setPlayedLocation(Player players)
+     {
+    	 gameboard.getPeg(savedLocation).addBead(players.getColour());
+    	 
+     }
+     
+     public void setPlayerLastPlayed()
+     {
+    	 human.setPlayerLastPlayed(savedLocation);
+    	 human.setPegClicked(true);
      }
 }
